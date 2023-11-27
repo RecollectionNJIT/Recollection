d28b8a18435acbc513975f93b7e739ee1807f639
jsingh0507 <122956356+jsingh0507@users.noreply.github.com>
2023-11-27 13:58:16 -0500

    detailView added update button

--- app/src/main/java/edu/njit/recollection/DetailsRemindersFragment.kt
+++ app/src/main/java/edu/njit/recollection/DetailsRemindersFragment.kt
@@ -5,9 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class DetailsRemindersFragment : Fragment() {
+
+    private lateinit var etEditTitle: EditText
+    private lateinit var etEditDescription: EditText
+    private lateinit var etEditDate: EditText
+    private lateinit var btnUpdateReminder: Button
+    private var reminder: Reminders? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -16,20 +25,46 @@ class DetailsRemindersFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_details_reminders, container, false)
 
         // Retrieve reminder data from intent
-        val reminder = activity?.intent?.getSerializableExtra("reminder") as Reminders?
+        reminder = activity?.intent?.getSerializableExtra("reminder") as Reminders?
+
+        // Initialize UI elements
+        etEditTitle = view.findViewById(R.id.etEditTitle)
+        etEditDescription = view.findViewById(R.id.etEditDescription)
+        etEditDate = view.findViewById(R.id.etEditDate)
+        btnUpdateReminder = view.findViewById(R.id.btnUpdateReminder)
 
         // Update UI with reminder details
-        val tvTitle = view.findViewById<TextView>(R.id.tvReminderTitleDetails)
-        val tvDescription = view.findViewById<TextView>(R.id.tvReminderDescriptionDetails)
-        val tvDate = view.findViewById<TextView>(R.id.tvReminderDateDetails)
-
         if (reminder != null) {
-            tvTitle.text = reminder.title
-            tvDescription.text = reminder.description
-            tvDate.text = reminder.date
+            etEditTitle.setText(reminder!!.title)
+            etEditDescription.setText(reminder!!.description)
+            etEditDate.setText(reminder!!.date)
+        }
+
+        // Set up click listener for the Update button
+        btnUpdateReminder.setOnClickListener {
+            updateReminder()
         }
 
         return view
+    }
+
+    private fun updateReminder() {
+        // Get updated values from EditText fields
+        val updatedTitle = etEditTitle.text.toString()
+        val updatedDescription = etEditDescription.text.toString()
+        val updatedDate = etEditDate.text.toString()
+
+        // Create a new instance of Reminders with updated values
+        reminder = reminder?.copy(
+            title = updatedTitle,
+            description = updatedDescription,
+            date = updatedDate
+        )
+
+        // Update UI with the edited values
+        etEditTitle.setText(updatedTitle)
+        etEditDescription.setText(updatedDescription)
+        etEditDate.setText(updatedDate)
     }
 
     companion object {
