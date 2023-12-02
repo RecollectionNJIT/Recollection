0604fcbfef1bb5a3136594be2dff29fe0f57f171
kk69njit <kk69@njit.edu>
2023-12-02 16:36:36 -0500

    Finance Homepage Finished

--- app/src/main/java/edu/njit/recollection/MainHomeFragment.kt
+++ app/src/main/java/edu/njit/recollection/MainHomeFragment.kt
@@ -1,19 +1,42 @@
 package edu.njit.recollection
 
 import android.content.Intent
+import android.icu.lang.UProperty.INT_START
+
 import android.os.Bundle
+import android.text.SpannableStringBuilder
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.charts.HorizontalBarChart
+import com.github.mikephil.charting.components.AxisBase
+import com.github.mikephil.charting.components.Description
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.formatter.ValueFormatter
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.database
+import java.text.DecimalFormat
 
 class MainHomeFragment : Fragment() {
     lateinit var calendarCV: CardView
     lateinit var financeCV: CardView
     lateinit var remindersCV: CardView
     lateinit var weatherCV: CardView
+
+    private val financeEntries = mutableListOf<FinanceEntry>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,7 +60,7 @@ class MainHomeFragment : Fragment() {
         weatherCV = view.findViewById(R.id.cvHomepageWeather)
 
         createCalendarCV()
-        createFinanceCV()
+        createFinanceCV(view)
         createRemindersCV()
         createWeatherCV()
 
@@ -46,7 +69,128 @@ class MainHomeFragment : Fragment() {
 
     fun createCalendarCV() {
     }
-    fun createFinanceCV() {
+    fun createFinanceCV(view: View) {
+        financeEntries.clear()
+        val auth = FirebaseAuth.getInstance()
+        val databaseRef = Firebase.database.reference
+        var filterMonth = ""
+        databaseRef.child("users").child(auth.uid!!).child("finances")
+            .orderByChild("date").limitToLast(1).addValueEventListener(object: ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    for (child in snapshot.children) {
+                        filterMonth = child.child("monthDate").getValue().toString()
+                    }
+                    databaseRef.child("users").child(auth.uid!!).child("finances")
+                        .orderByChild("date").addValueEventListener(object: ValueEventListener {
+                            override fun onDataChange(snapshot: DataSnapshot) {
+                                for (child in snapshot.children) {
+                                    if (child.child("monthDate").getValue().toString() != filterMonth)
+                                        continue
+                                    val financeEntry = FinanceEntry(
+                                        child.child("date").getValue().toString(),
+                                        filterMonth,
+                                        child.child("type").getValue().toString(),
+                                        child.child("category").getValue().toString(),
+                                        child.child("amount").getValue().toString().toDouble(),
+                                        child.key
+                                    )
+                                    financeEntries.add(financeEntry)
+                                }
+                                createBarChart(view, filterMonth)
+                            }
+                            override fun onCancelled(error: DatabaseError) {
+                                Log.e("firebaseFinanceHomepage2", "error", Throwable(error.toString()))
+                            }
+                        })
+                }
+                override fun onCancelled(error: DatabaseError) {
+                    Log.e("firebaseFinanceHomepage1", "error", Throwable(error.toString()))
+                }
+            })
+
+    }
+    fun createBarChart(view: View, filterMonth: String) {
+        val barChart = view.findViewById<HorizontalBarChart>(R.id.homepageBarChart)
+
+        val description = Description()
+        description.text = filterMonth
+        description.setPosition(720f, 65f)
+        description.textSize = 20f
+        description.text
+        description.textColor = ContextCompat.getColor(view.context, R.color.teal)
+        barChart.description = description
+
+        barChart.legend.isEnabled = false
+        barChart.setPinchZoom(false)
+        barChart.setScaleEnabled(false)
+        barChart.setDrawValueAboveBar(false)
+
+        barChart.xAxis.setDrawGridLines(false)
+        barChart.xAxis.position = XAxis.XAxisPosition.BOTTOM
+        barChart.xAxis.isEnabled = true
+        barChart.xAxis.setDrawAxisLine(false)
+        barChart.xAxis.textSize = 12f
+        barChart.xAxis.textColor = ContextCompat.getColor(view.context, R.color.teal)
+
+        barChart.axisLeft.isEnabled = false
+        barChart.axisLeft.setDrawZeroLine(true)
+        barChart.axisLeft.granularity = 1f
+        barChart.axisLeft.axisMinimum = 0f
+
+        barChart.axisRight.setDrawAxisLine(true)
+        barChart.axisRight.setDrawGridLines(false)
+        barChart.axisRight.isEnabled = false
+
+        barChart.xAxis.setLabelCount(3, false)
+        val formatterX: ValueFormatter = object : ValueFormatter() {
+            override fun getAxisLabel(value: Float, axis: AxisBase): String {
+                return when (value) {
+                    0f -> "Expenses"
+                    .5f -> "Income"
+                    else -> ""
+                }
+            }
+        }
+        barChart.xAxis.valueFormatter = formatterX
+
+        var expenseTotal = 0.0
+        var incomeTotal = 0.0
+        for (entry in financeEntries) {
+            if (entry.type == "Expense")
+                expenseTotal += entry.amount!!
+            else
+                incomeTotal += entry.amount!!
+        }
+        val barEntries: ArrayList<BarEntry> = ArrayList()
+        barEntries.add(BarEntry(0f, expenseTotal.toFloat()))
+        barEntries.add(BarEntry(.5f, incomeTotal.toFloat()))
+
+        val dataset = BarDataSet(barEntries, "Monthly Income & Expenses")
+        dataset.valueTextSize = 12f
+        dataset.setColors(
+            ContextCompat.getColor(view.context, R.color.burgandy),
+            ContextCompat.getColor(view.context, R.color.forest_green)
+        )
+        dataset.setValueTextColors(mutableListOf(ContextCompat.getColor(view.context, R.color.white), ContextCompat.getColor(view.context, R.color.white)))
+        val formatterDataSet: ValueFormatter = object : ValueFormatter() {
+            val mFormat = DecimalFormat("###,###,##0.00")
+            override fun getFormattedValue(value: Float): String? {
+                return if (value == 0f) {
+                    ""
+                }
+                else
+                    return "$" + mFormat.format(value.toDouble())
+            }
+        }
+        dataset.valueFormatter = formatterDataSet
+
+        val data = BarData(dataset)
+        data.barWidth = .4f
+        barChart.data = data
+
+        barChart.animateY(1400, Easing.EaseInOutQuad)
+
+        barChart.invalidate()
     }
     fun createRemindersCV() {
     }
